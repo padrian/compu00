e8a26ea06be9bc7fa1e48c6befde2b6bdc44205f
Baba Padrian <padrian.baba@gmail.com>
2018-11-01 21:30:30 +0300

    initial commit 00

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -5,6 +5,7 @@
 
   apt-get update
   apt-get install -y git python-dev python-pip python-openssl
-  pip install --upgrade pip
-  pip install -U setuptools
-  pip install ansible
+  sudo apt-add-repository ppa:ansible/ansible --yes
+  sudo apt update
+  sudo apt install ansible --yes
+
