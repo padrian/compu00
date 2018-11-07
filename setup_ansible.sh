1150af9b073a2e0e6fcedd357fc585cf80301b43
Baba Padrian <padrian.baba@gmail.com>
2018-11-07 20:43:59 +0300

    new virtual host config

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+export LC_ALL=C
 # Install ansible
 
   apt-get update
@@ -9,3 +9,6 @@
   sudo apt update
   sudo apt install ansible --yes
 
+# boto for s3 backups
+  pip install boto3
+  
